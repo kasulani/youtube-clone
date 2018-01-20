e9645644466f7dd4cb371eb50ce591d4658de2e4
kasulani <kasulani@gmail.com>
2018-01-21 00:23:04 +0300

    fixes a typo in the jsx

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -24,7 +24,7 @@ export default class NavBar extends Component {
                     loading={isLoading}
                     onSearchChange={this.onSearchChange}
                     value={term}
-                />;
+                />
             </nav>
         );
     }
